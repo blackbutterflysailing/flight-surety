1e66652c966ec3e88c1a9654e7948c1f0af4c1e4
Marie E Patterson <vikingway2deal@gmail.com>
2020-01-06 21:29:38 -0500

    master title: Converted function to be async to use await commands.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,22 +2,23 @@ const FlightSuretyApp = artifacts.require("FlightSuretyApp");
 const FlightSuretyData = artifacts.require("FlightSuretyData");
 const fs = require('fs');
 
-module.exports = function(deployer) {
+module.exports = async function(deployer, network, accounts) {
 
-    let firstAirline = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
-    deployer.deploy(FlightSuretyData)
-    .then(() => {
-        return deployer.deploy(FlightSuretyApp)
-                .then(() => {
-                    let config = {
-                        localhost: {
-                            url: 'http://localhost:8545',
-                            dataAddress: FlightSuretyData.address,
-                            appAddress: FlightSuretyApp.address
-                        }
-                    }
-                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                });
-    });
+    let firstAirline = accounts[1];
+    await deployer.deploy(FlightSuretyData);
+    const flightSuretyDataContract = await FlightSuretyData.deployed();
+
+    await deployer.deploy(FlightSuretyApp, flightSuretyDataContract.address);
+    const flightSuretyAppContract = await FlightSuretyApp.deployed();
+
+    let config = {
+        localhost: {
+            url: 'http://localhost:8545',
+            dataAddress: FlightSuretyData.address,
+            appAddress: FlightSuretyApp.address
+        }
+    }    
+
+    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+  fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
 }
