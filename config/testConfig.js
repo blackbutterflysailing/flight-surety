2d4295efbbe1797942d95dac78ec0c0173a54639
Marie E Patterson <vikingway2deal@gmail.com>
2020-01-06 21:48:31 -0500

    master title: Removed passing firstAirline into the FlightSuretyApp.new constructor.

--- config/testConfig.js
+++ config/testConfig.js
@@ -22,10 +22,10 @@ var Config = async function(accounts) {
 
 
     let owner = accounts[0];
-    let firstAirline = accounts[1];
+    let firstAirline = accounts[0];
 
-    let flightSuretyData = await FlightSuretyData.new();
-    let flightSuretyApp = await FlightSuretyApp.new(flightSuretyData.address, firstAirline);
+    let flightSuretyData = await FlightSuretyData.new({from: owner});
+    let flightSuretyApp = await FlightSuretyApp.new(flightSuretyData.address, {from: owner});
 
     let airlinesByProxy = accounts.slice(1, 4);
     let airlinesByVoting = accounts.slice(4, 11);
