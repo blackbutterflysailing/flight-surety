87b5eb15bfe685dbb1a64f38579abc1874296de4
Marie E Patterson <vikingway2deal@gmail.com>
2020-01-10 23:51:42 -0500

    master title: Fixed major problem of error 'Error: the tx doesn't have the correct nonce. account has nonce of: 7 tx has nonce of: 6'  I added a new development configuration in the networks section and commented out the previous on that had HDWalletProvide as the provider.  This resolved the error.

--- truffle-config.js
+++ truffle-config.js
@@ -4,8 +4,14 @@ var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble s
 module.exports = {
   networks: {
     development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: 6721975
+    },
+    developmentOld: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50, false);
       },
       network_id: '*',
       gas: 9999998
