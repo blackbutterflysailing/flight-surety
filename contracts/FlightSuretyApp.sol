25f9c8f5ab9af85ef5879eb0658dd7c600718676
Marie E Patterson <vikingway2deal@gmail.com>
2020-01-12 23:41:38 -0500

    master title: use msg.sender instead of variable passengerAddress.

--- contracts/FlightSuretyApp.sol
+++ contracts/FlightSuretyApp.sol
@@ -362,7 +362,6 @@ contract FlightSuretyApp {
 
     function purchaseFlightInsurance
     (
-        address passengerAddress,
         address airlineAddress,
         string calldata flightName,
         uint256 departureTime,
@@ -377,14 +376,14 @@ contract FlightSuretyApp {
         require(flights[flightKey].isRegistered, "Flight has not been registered");
 
         flightSuretyData.buyFlightInsurance.value(msg.value)(
-            passengerAddress,
+            msg.sender,
             airlineAddress,
             flightName,
             departureTime,
             insuranceCost
         );
 
-        emit FlightInsurancePurchased(airlineAddress, passengerAddress, flightName);
+        emit FlightInsurancePurchased(airlineAddress, msg.sender, flightName);
     }
 // region ORACLE MANAGEMENT
 
@@ -480,13 +479,13 @@ contract FlightSuretyApp {
                         external
     {
         require(
-            (oracles[msg.sender].indexes[0] == index) || 
-            (oracles[msg.sender].indexes[1] == index) || 
+            (oracles[msg.sender].indexes[0] == index) ||
+            (oracles[msg.sender].indexes[1] == index) ||
             (oracles[msg.sender].indexes[2] == index),
             "Index does not match oracle request");
 
 
-        bytes32 key = keccak256(abi.encodePacked(index, airline, flight, timestamp)); 
+        bytes32 key = keccak256(abi.encodePacked(index, airline, flight, timestamp));
         require(oracleResponses[key].isOpen, "Flight or timestamp do not match oracle request");
 
         oracleResponses[key].responses[statusCode].push(msg.sender);
